95d89737b45213aafe9a3045570769887adc6202
Saloncey <miedzyniebem@gmail.com>
2025-08-02 16:55:00 +0200

    GithubService changes - from RestTemplate to RestClient

--- src/main/java/dev/salonce/atiperatask/services/GithubService.java
+++ src/main/java/dev/salonce/atiperatask/services/GithubService.java
@@ -5,10 +5,13 @@ import dev.salonce.atiperatask.dtos.GithubRepositoryDto;
 import dev.salonce.atiperatask.exceptions.UserNotFoundException;
 import dev.salonce.atiperatask.models.Branch;
 import dev.salonce.atiperatask.models.GithubRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
@@ -19,49 +22,45 @@ import java.util.List;
 @Service
 public class GithubService {
 
-    @Value("${github.token}")
-    private String githubToken;
+    public GithubService(RestClient.Builder restClientBuilder, @Value("${github.token}") String githubToken){
+        this.restClient = restClientBuilder
+                .baseUrl("https://api.github.com")
+                .defaultHeader("Authorization", "Bearer " + githubToken)
+                .defaultHeader("Accept", "application/vnd.github.v3+json")
+                .build();
 
-    private final RestTemplate restTemplate = new RestTemplate();
+        this.githubToken = githubToken;
+    }
+
+    @Value("${github.token}")
+    private final String githubToken;
+
+    private final RestClient restClient;
+
     private final String GITHUB_API = "https://api.github.com";
 
     private List<GithubRepositoryDto> getUserRepositories(String username) {
-        String url = GITHUB_API + "/users/" + username + "/repos";
-
-        try {
-            ResponseEntity<GithubRepositoryDto[]> response = restTemplate.getForEntity(url, GithubRepositoryDto[].class);
-            GithubRepositoryDto[] reposArray = response.getBody();
-            if (reposArray == null) return Collections.emptyList();
-            return Arrays.asList(reposArray);
-        } catch (HttpClientErrorException.NotFound e) {
-            throw new UserNotFoundException("User '" + username + "' not found.");
-        }
+        GithubRepositoryDto[] repos = restClient
+                .get()
+                .uri("/users/" + username + "/repos")
+                .retrieve()
+                .onStatus(HttpStatus.NOT_FOUND::equals, (status, response) -> {
+                    throw new UserNotFoundException(username);
+                })
+                .body(GithubRepositoryDto[].class);
+        if (repos == null) return Collections.emptyList();
+        return Arrays.asList(repos);
     }
 
+    private List<BranchDto> getBranches(String username, String repoName) {
+            BranchDto[] branchesArray = restClient
+                    .get()
+                    .uri("/repos/" + username + "/" + repoName + "/branches")
+                    .retrieve()
+                    .body(BranchDto[].class);
 
-
-    private List<BranchDto> getBranches(String url) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "Bearer " + githubToken);
-        headers.set("Accept", "application/vnd.github+json");
-
-        HttpEntity<Void> entity = new HttpEntity<>(headers);
-
-        try {
-            ResponseEntity<BranchDto[]> response = restTemplate.exchange(
-                    url,
-                    HttpMethod.GET,
-                    entity,
-                    BranchDto[].class
-            );
-
-            BranchDto[] branchesArray = response.getBody();
             if (branchesArray == null) return Collections.emptyList();
             return Arrays.asList(branchesArray);
-        } catch (Exception e) {
-            System.out.println("Error fetching branches");
-            return List.of();
-        }
     }
 
 
@@ -72,19 +71,17 @@ public class GithubService {
         for(GithubRepositoryDto githubRepositoryDto : userGithubRepos){
             if (githubRepositoryDto.getFork()) continue;
 
-            String name = githubRepositoryDto.getName();
+            String repoName = githubRepositoryDto.getName();
             String ownerLogin = githubRepositoryDto.getOwner().getLogin();
 
-            String branches_url = GITHUB_API + "/repos/" + username + "/" + name + "/branches";
-
-            List<BranchDto> branchDtos = getBranches(branches_url);
+            List<BranchDto> branchDtos = getBranches(username, repoName);
 
             List<Branch> branches = new ArrayList<>();
             for (BranchDto branchDto : branchDtos){
                 branches.add(new Branch(branchDto.getName(), branchDto.getCommitDto().getSha()));
             }
 
-            GithubRepository githubRepository = new GithubRepository(name, ownerLogin, branches);
+            GithubRepository githubRepository = new GithubRepository(repoName, ownerLogin, branches);
             githubRepositoryList.add(githubRepository);
         }
         return githubRepositoryList;
