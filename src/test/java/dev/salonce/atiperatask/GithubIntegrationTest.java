732944c6b907303eda3f65509432698b899b79ee
Saloncey <miedzyniebem@gmail.com>
2025-08-05 15:55:00 +0200

    added webflux for testing scope, so that WebTestClient works

--- src/test/java/dev/salonce/atiperatask/GithubIntegrationTest.java
+++ src/test/java/dev/salonce/atiperatask/GithubIntegrationTest.java
@@ -2,17 +2,12 @@ package dev.salonce.atiperatask;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.client.WireMock;
-import dev.salonce.atiperatask.models.Branch;
-import dev.salonce.atiperatask.models.GithubRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
