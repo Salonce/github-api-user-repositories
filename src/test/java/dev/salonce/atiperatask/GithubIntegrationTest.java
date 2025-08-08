86b0a8cd969dad8a0e76a387d64a962564456aff
Saloncey <miedzyniebem@gmail.com>
2025-08-08 23:38:15 +0200

    works but i don't think correct

--- src/test/java/dev/salonce/atiperatask/GithubIntegrationTest.java
+++ src/test/java/dev/salonce/atiperatask/GithubIntegrationTest.java
@@ -2,24 +2,36 @@ package dev.salonce.atiperatask;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.client.WireMock;
-import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.wiremock.spring.EnableWireMock;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
-@WireMockTest(httpPort = 8081)
 class GithubIntegrationTest {
 
+    @RegisterExtension
+    static WireMockExtension wireMock = WireMockExtension.newInstance()
+            .options(WireMockConfiguration.options().dynamicPort())
+            .build();
+
     @DynamicPropertySource
-    static void configure(DynamicPropertyRegistry registry) {
-        registry.add("github.base-url", () -> "http://localhost:8081");
+    static void overrideGithubBaseUrl(DynamicPropertyRegistry registry) {
+        System.out.println("WireMock server host: " + wireMock.baseUrl());
+        System.out.println("WireMock server port: " + wireMock.getPort());
+        registry.add("github.base-url", () ->  wireMock.baseUrl());
     }
 
     @Autowired
@@ -31,7 +43,7 @@ class GithubIntegrationTest {
     @BeforeEach
     void setupStubs() {
         // Mock GitHub API call
-        WireMock.stubFor(WireMock.get(WireMock.urlEqualTo("/users/testuser/repos"))
+        wireMock.stubFor(WireMock.get(WireMock.urlEqualTo("/users/testuser/repos"))
                 .willReturn(WireMock.aResponse()
                         .withStatus(200)
                         .withHeader("Content-Type", "application/json")
@@ -69,7 +81,7 @@ class GithubIntegrationTest {
                             """)));
 
         // Mock branches for non-fork repo
-        WireMock.stubFor(WireMock.get(WireMock.urlEqualTo("/repos/testuser/sample-api/branches"))
+        wireMock.stubFor(WireMock.get(WireMock.urlEqualTo("/repos/testuser/sample-api/branches"))
                 .willReturn(WireMock.aResponse()
                         .withStatus(200)
                         .withHeader("Content-Type", "application/json")
@@ -87,7 +99,7 @@ class GithubIntegrationTest {
                             ]
                             """)));
         // forked repo - shouldn't appear
-        WireMock.stubFor(WireMock.get(WireMock.urlEqualTo("/repos/testuser/sample-api2/branches"))
+        wireMock.stubFor(WireMock.get(WireMock.urlEqualTo("/repos/testuser/sample-api2/branches"))
                 .willReturn(WireMock.aResponse()
                         .withStatus(200)
                         .withHeader("Content-Type", "application/json")
@@ -100,7 +112,7 @@ class GithubIntegrationTest {
                               }
                             ]
                             """)));
-        WireMock.stubFor(WireMock.get(WireMock.urlEqualTo("/repos/testuser/sample-api3/branches"))
+        wireMock.stubFor(WireMock.get(WireMock.urlEqualTo("/repos/testuser/sample-api3/branches"))
                 .willReturn(WireMock.aResponse()
                         .withStatus(200)
                         .withHeader("Content-Type", "application/json")
