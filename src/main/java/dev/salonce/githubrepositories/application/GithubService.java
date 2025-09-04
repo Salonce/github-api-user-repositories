3f0fc15c51156fd33443fc2be1d5b3d9079d9cbd
Saloncey <miedzyniebem@gmail.com>
2025-09-04 02:18:59 +0200

    added githubrestclient

--- src/main/java/dev/salonce/githubrepositories/application/GithubService.java
+++ src/main/java/dev/salonce/githubrepositories/application/GithubService.java
@@ -6,6 +6,7 @@ import dev.salonce.githubrepositories.exceptions.NullDataException;
 import dev.salonce.githubrepositories.exceptions.UserNotFoundException;
 import dev.salonce.githubrepositories.domain.Branch;
 import dev.salonce.githubrepositories.domain.GithubRepository;
+import dev.salonce.githubrepositories.infrastructure.GithubRestClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
@@ -18,45 +19,14 @@ import java.util.List;
 @Service
 public class GithubService {
 
-    public GithubService(RestClient.Builder restClientBuilder, @Value("${github.token}") String githubToken, @Value("${github.base-url}") String githubBaseUrl){
-        this.restClient = restClientBuilder
-                .baseUrl(githubBaseUrl)
-                .defaultHeader("Authorization", "Bearer " + githubToken)
-                .defaultHeader("Accept", "application/vnd.github.v3+json")
-                .build();
+    public final GithubRestClient githubRestClient;
 
-        this.githubToken = githubToken;
-    }
-
-    @Value("${github.token}")
-    private final String githubToken;
-    private final RestClient restClient;
-
-    private List<GithubRepositoryDto> getUserRepositories(String username) {
-        GithubRepositoryDto[] repos = restClient
-                .get()
-                .uri("/users/" + username + "/repos")
-                .retrieve()
-                .onStatus(HttpStatus.NOT_FOUND::equals, (status, response) -> {
-                    throw new UserNotFoundException("User " + username + " not found.");
-                })
-                .body(GithubRepositoryDto[].class);
-        if (repos == null) throw new NullDataException("Incoming data is wrong. The repository returned a null value.");
-        return Arrays.asList(repos);
-    }
-
-    private List<BranchDto> getBranches(String username, String repoName) {
-            BranchDto[] branchesArray = restClient
-                    .get()
-                    .uri("/repos/" + username + "/" + repoName + "/branches")
-                    .retrieve()
-                    .body(BranchDto[].class);
-            if (branchesArray == null) throw new NullDataException("Incoming data is wrong. Branches returned a null value.");
-            return Arrays.asList(branchesArray);
+    public GithubService(GithubRestClient githubRestClient){
+        this.githubRestClient = githubRestClient;
     }
 
     public List<GithubRepository> getRepositoriesInformation(String username){
-        List<GithubRepositoryDto> userGithubRepos = getUserRepositories(username);
+        List<GithubRepositoryDto> userGithubRepos = githubRestClient.getUserRepositories(username);
 
         List<GithubRepository> githubRepositoryList = new ArrayList<>();
         for(GithubRepositoryDto githubRepositoryDto : userGithubRepos){
@@ -65,7 +35,7 @@ public class GithubService {
             String repoName = githubRepositoryDto.name();
             String ownerLogin = githubRepositoryDto.ownerDto().login();
 
-            List<BranchDto> branchDtos = getBranches(username, repoName);
+            List<BranchDto> branchDtos = githubRestClient.getBranches(username, repoName);
 
             List<Branch> branches = new ArrayList<>();
             for (BranchDto branchDto : branchDtos){
---
+++ src/main/java/dev/salonce/githubrepositories/infrastructure/GithubRestClient.java
@@ -0,0 +1,49 @@
+package dev.salonce.githubrepositories.infrastructure;
+
+import dev.salonce.githubrepositories.dtos.BranchDto;
+import dev.salonce.githubrepositories.dtos.GithubRepositoryDto;
+import dev.salonce.githubrepositories.exceptions.NullDataException;
+import dev.salonce.githubrepositories.exceptions.UserNotFoundException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class GithubRestClient {
+
+    public GithubRestClient(RestClient.Builder restClientBuilder, @Value("${github.token}") String githubToken, @Value("${github.base-url}") String githubBaseUrl){
+        this.restClient = restClientBuilder
+                .baseUrl(githubBaseUrl)
+                .defaultHeader("Authorization", "Bearer " + githubToken)
+                .defaultHeader("Accept", "application/vnd.github.v3+json")
+                .build();
+    }
+    private final RestClient restClient;
+
+    public List<GithubRepositoryDto> getUserRepositories(String username) {
+        GithubRepositoryDto[] repos = restClient
+                .get()
+                .uri("/users/" + username + "/repos")
+                .retrieve()
+                .onStatus(HttpStatus.NOT_FOUND::equals, (status, response) -> {
+                    throw new UserNotFoundException("User " + username + " not found.");
+                })
+                .body(GithubRepositoryDto[].class);
+        if (repos == null) throw new NullDataException("Incoming data is wrong. The repository returned a null value.");
+        return Arrays.asList(repos);
+    }
+
+    public List<BranchDto> getBranches(String username, String repoName) {
+        BranchDto[] branchesArray = restClient
+                .get()
+                .uri("/repos/" + username + "/" + repoName + "/branches")
+                .retrieve()
+                .body(BranchDto[].class);
+        if (branchesArray == null) throw new NullDataException("Incoming data is wrong. Branches returned a null value.");
+        return Arrays.asList(branchesArray);
+    }
+}
